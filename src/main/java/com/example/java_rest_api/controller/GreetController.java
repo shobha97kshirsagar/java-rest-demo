d068a516dd4397d4bf2b1086d075f208712d909f
priyanka <priyabuss2004@gmail.com>
2025-07-28 16:49:14 +0530

    controller updated

--- src/main/java/com/example/java_rest_api/controller/GreetController.java
+++ src/main/java/com/example/java_rest_api/controller/GreetController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetController {
 
-    @GetMapping("/welcome")
+    @GetMapping("/greet")
     public String getMessage(){
         return "<h1>  Hello from Spring Rest Controller </h1>";
     }
