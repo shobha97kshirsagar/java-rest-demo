12e0a353c0defea9911f5c09b0116a8ef2942f92
shobha97kshirsagar <kshirsagarshobha97@gmail.com>
2025-09-06 10:21:36 +0530

    Update JavaRestApiApplication.java

--- src/main/java/com/example/java_rest_api/JavaRestApiApplication.java
+++ src/main/java/com/example/java_rest_api/JavaRestApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaRestApiApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Java springboot");
+		System.out.println("Hello from Java springbootttttttt");
 		SpringApplication.run(JavaRestApiApplication.class, args);
 	}
 
